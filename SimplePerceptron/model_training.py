5beb55870cc7fc66ef1f719133c1673d8a5af5ae
Witold Frącek <witek.fracek@gmail.com>
2022-10-08 21:22:51 +0200

    model changes: bias as parameter - not in weights matrix

--- SimplePerceptron/model_training.py
+++ SimplePerceptron/model_training.py
@@ -1,17 +1,27 @@
 from simple_perceptron import SimplePerceptron
 from perceptron_utils import *
+import matplotlib.pyplot as pyl
 
-TRAIN_SIZE = 1000
-ITERATIONS = 1000
-ALPHA = 0.5
+SET_SIZE = 100
+TRAIN_PERCENTAGE = 0.8
+ITERATIONS = 100_000
+ALPHA = 0.2
 
 
 def main():
-    x_train, y_train = generate_training_set(TRAIN_SIZE, OR_VALUES)
+    train_last_index = int(SET_SIZE * TRAIN_PERCENTAGE)
+    x_ext, y_ext = generate_training_set(SET_SIZE, OR_VALUES)
+    x_train, x_test = x_ext[..., :train_last_index], x_ext[..., train_last_index:]
+    y_train, y_test = y_ext[..., :train_last_index], y_ext[..., train_last_index:]
+    pyl.scatter(x_train[0], x_train[1])
+    pyl.show()
     perceptron = SimplePerceptron(x_train, y_train, unipolar_activation, step_function_delta)
     perceptron.train_model(ITERATIONS, ALPHA)
-    res = perceptron.check_sample(-1, 1)
-    print(res)
+    print(f"Train accuracy: {perceptron.train_accuracy}")
+    print(f"Weights:  {perceptron.weights}")
+    print(f"Bias: {perceptron.bias}")
+    perceptron.test_model(x_test, y_test)
+    print(f"Test accuracy: {perceptron.train_accuracy}")
 
 
 if __name__ == '__main__':
--- SimplePerceptron/simple_perceptron.py
+++ SimplePerceptron/simple_perceptron.py
@@ -5,52 +5,79 @@ import random
 class SimplePerceptron:
     def __init__(self, x_train: np.ndarray, y_train: np.ndarray, activation_function, gradient, output_mapping=lambda x: x):
         self.__weights = np.zeros(1)
+        self.__bias = random.random()
         self.__x_train = x_train
         self.__y_train = y_train
         self.__init_weights(x_train.shape[0])
         self.__act_function = activation_function
         self.__gradient = gradient
+        self.__act_derivative = lambda x: x
         self.__mapping = np.vectorize(output_mapping)
         self.__is_trained = False
         self.__accuracy = 0
+        self.__test_accuracy = 0
 
     def __init_weights(self, input_size: int):
         self.__weights = np.random.randn(1, input_size) * 0.001
 
-    @staticmethod
-    def __cost(w, x):
-        return w.dot(x)
+    def __cost(self, w, x):
+        return w.dot(x) + self.__bias
 
     def __activation(self, cost):
-        return self.__act_function(cost)
+        return self.__act_function(cost, theta=0)
 
-    def __get_accuracy(self, predictions) -> float:
-        transformed = self.__mapping(predictions)
-        total = self.__y_train.shape[1]
-        counter = 0
-        for pred, exp in zip(transformed[0], self.__y_train[0]):
-            if pred == exp:
-                counter += 1
-        return counter / total
+    def __error(self, predictions):
+        return self.__y_train - predictions
+
+    def __get_accuracy(self, expected, predictions) -> float:
+        # transformed = self.__mapping(predictions)
+        # total = expected.shape[1]
+        mean = np.mean(expected == predictions)
+        return float(mean)
+        # counter = 0
+        # for pred, exp in zip(transformed[0], expected[0]):
+        #     if pred == exp:
+        #         counter += 1
+        # return counter / total
 
     def train_model(self, iterations, learning_rate: float):
         activation = np.zeros(self.__y_train.shape[1])
         for _ in range(iterations):
             current_cost = self.__cost(self.__weights, self.__x_train)
             activation = self.__activation(current_cost)
-            delta = self.__gradient(activation, self.__y_train, self.__x_train)
-            self.__weights = self.__weights + learning_rate * delta
-        self.__accuracy = self.__get_accuracy(activation)
+            error = self.__error(activation)
+            dw = self.__act_derivative(error).dot(self.__x_train.T)
+            db = np.sum(self.__act_derivative(error)) / self.__x_train.shape[1]
+            #delta = self.__gradient(activation, self.__y_train, self.__x_train)
+            self.__weights = self.__weights + learning_rate * dw
+            self.__bias = self.__bias + learning_rate * db
+        self.__accuracy = self.__get_accuracy(self.__y_train, activation)
         self.__is_trained = True
 
-    def check_sample(self, x1: float, x2: float):
+    def test_model(self, x_test: np.ndarray, y_test: np.ndarray):
         if not self.__is_trained:
             raise Exception("Model was not trained. Please train the model before using it.")
-        sample = np.array([1, x1, x2]).T
-        current_cost = self.__cost(self.__weights, sample)
+        current_cost = self.__cost(self.__weights, x_test)
         activation = self.__activation(current_cost)
-        return activation
+        self.__test_accuracy = self.__get_accuracy(y_test, activation)
 
+    @property
+    def train_accuracy(self):
+        return self.__accuracy
+
+    @property
+    def test_accuracy(self):
+        if not self.__is_trained:
+            raise Exception("Model was not trained. Please train the model before using it.")
+        return self.__test_accuracy
+
+    @property
+    def weights(self):
+        return self.__weights
+
+    @property
+    def bias(self):
+        return self.__bias
 
 
 
