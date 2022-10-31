89454b7242eff28d36b7c8d37721dcaacf91add9
Witold Frącek <witek.fracek@gmail.com>
2022-10-31 22:42:20 +0100

    working numbers

--- MultilayerNetwork/main.py
+++ MultilayerNetwork/main.py
@@ -1,35 +1,52 @@
 from neural_network import NeuralNetwork
-from utils import relu, softmax, relu_der, sigmoid, sigmoid_der, tanh, tanh_der
+from utils import relu, softmax, relu_der, sigmoid, sigmoid_der, tanh, tanh_der, load_mnist_data
 import numpy as np
 from binary_utils import *
 from keras.datasets import mnist
 import tensorflow
 from tqdm import tqdm
+import matplotlib.pyplot as pyl
 
 np.set_printoptions(precision=3, suppress=True)
+np.seterr(divide='ignore', invalid='ignore')
 
-LAYER_SIZES = [(2, 2), (2, 2)]
-ACT_FUNCTIONS = [relu] * len(LAYER_SIZES)
+LAYER_SIZES = [(28 * 28, 50), (50, 30), (30, 20), (20, 10)]
+ACT_FUNCTIONS = [tanh] * len(LAYER_SIZES)
 ACT_FUNCTIONS[-1] = softmax
-ACT_DERIVATIVES = [relu_der] * len(LAYER_SIZES)
-LEARNING_RATE = 0.5
-ITERATIONS = 10_000
+ACT_DERIVATIVES = [tanh_der] * len(LAYER_SIZES)
+LEARNING_RATE = 0.3
+ITERATIONS = 1000
 
 
 def main():
     nn = NeuralNetwork(LAYER_SIZES,
                        ACT_FUNCTIONS,
                        ACT_DERIVATIVES,
-                       learning_rate=LEARNING_RATE,
-                       init_weights_range=1)
+                       learning_rate=LEARNING_RATE)
 
-    x, y = generate_set(1000, XOR_VALUES, operator_xor)
+    # x, y = generate_set(1000, XOR_VALUES, operator_xor)
+    x_train, y_train, x_test, y_test = load_mnist_data()
     for _ in tqdm(range(ITERATIONS), colour='GREEN'):
-        a = nn.forward(x)
-        nn.backward(y)
+        a = nn.forward(x_train)
+        nn.backward(y_train)
         nn.update()
-    print(x[..., :7])
-    print(a[..., :7])
+    print(y_train[:, :7])
+    print(a[:, :7])
+    eg = x_train.reshape(28, 28, -1).T
+    pyl.imshow(eg[0], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[1], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[2], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[3], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[4], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[5], cmap="Greys")
+    pyl.show()
+    pyl.imshow(eg[6], cmap="Greys")
+    pyl.show()
 
 
 if __name__ == '__main__':
--- MultilayerNetwork/utils.py
+++ MultilayerNetwork/utils.py
@@ -30,3 +30,30 @@ def tanh_der(value: np.ndarray) -> np.ndarray:
 def softmax(values: np.ndarray) -> np.ndarray:
     return np.exp(values) / np.sum(np.exp(values), axis=0)
 
+
+def load_mnist_data():
+    from keras.datasets import mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.T.reshape(28 * 28, -1) / 255
+    x_test = x_test.T.reshape(28 * 28, -1) / 255
+
+    temp = []
+    for elem in y_train:
+        col = transform_row(elem)
+        temp.append(col)
+    new_y_train = np.column_stack(temp)
+
+    temp = []
+    for elem in y_test:
+        col = transform_row(elem)
+        temp.append(col)
+    new_y_test = np.column_stack(temp)
+
+    return x_train, new_y_train, x_test, new_y_test
+
+
+def transform_row(index):
+    ret = np.zeros((10, 1))
+    ret[index] = 1
+    return ret
